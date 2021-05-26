78186e2445ad6f1b302d3a5f8c2f0cc1a4bfdb16
Artem Baranov <temoncher@yandex.ru>
2021-05-26 14:08:21 +0300

    extract temperature formatting into separate function

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -9,19 +9,22 @@ interface WeatherCardProps {
 }
 
 const ICON_URL_ROOT = 'http://openweathermap.org/img/wn/'
+const formatTemperature = (temperature: number): string => {
+  const roundedTemperature = Math.round(temperature)
 
-const WeatherCard: React.FC<WeatherCardProps> = (props) => {
-  const date = React.useMemo<Date>(() => new Date(props.date * 1000), [props.date])
-  const roundedTemperature = Math.round(props.temperature)
-  const temperature = roundedTemperature >= 0 ? `+${roundedTemperature}°` : `${roundedTemperature}°`
+  if (roundedTemperature === 0) {
+    return `${roundedTemperature}°`
+  }
 
-  return (
-    <div className="weather-card">
-      <span className="weather-card__date text-subtitle">{formatDayMonthYear(date)}</span>
-      <img className="weather-card__image" src={`${ICON_URL_ROOT}${props.icon}@2x.png`} alt="weather" />
-      <span className="weather-card__temperature">{temperature}</span>
-    </div>
-  )
+  return roundedTemperature >= 0 ? `+${roundedTemperature}°` : `${roundedTemperature}°`
 }
+
+const WeatherCard: React.FC<WeatherCardProps> = (props) => (
+    <div className="weather-card">
+      <span className="weather-card__date text-subtitle">{formatDayMonthYear(new Date(props.date * 1000))}</span>
+      <img className="weather-card__image" src={`${ICON_URL_ROOT}${props.icon}@2x.png`} alt="weather" />
+      <span className="weather-card__temperature">{formatTemperature(props.temperature)}</span>
+    </div>
+)
 
 export default WeatherCard
